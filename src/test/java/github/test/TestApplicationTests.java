d458052579e2c8cf3866baa071aa65a00fd7ac03
Choonsik <moodeary@gmail.com>
2022-08-22 11:28:20 +0900

    spring initializr

---
+++ src/test/java/github/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package github.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
